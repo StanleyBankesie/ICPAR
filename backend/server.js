0232ca4ca67571443b557c45c240e727b03ab344
STANLEY BANKESIE <stanleyetornam@gmail.com>
2025-05-07 14:21:10 +0000

    updated changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// server.js
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: __dirname + "/.env" });
 }
@@ -15,16 +14,28 @@ const app = express();
 // Middleware
 
 const corsOptions = {
-  origin: ["https://icpar.vercel.app", "https://www.icpar.vercel.app"],
+  origin: "https://www.icpar.vercel.app",
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
 };
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://www.icpar.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
 //app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 // database function invocation
 connectToDB();
 
