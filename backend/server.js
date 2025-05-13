2c5dc911fef83f322b8a12daeaaba9186b261845
STANLEY BANKESIE <stanleyetornam@gmail.com>
2025-05-13 16:18:55 +0000

    fresh update

--- backend/server.js
+++ backend/server.js
@@ -15,30 +15,37 @@ const app = express();
 dotenv.config();
 
 // Middleware: CORS configuration
-const corsOptions = {
-  origin: "https://icpar.vercel.app",
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-  credentials: true,
-};
+//const corsOptions = {
+// origin: "https://icpar.vercel.app",
+//  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+//  credentials: true,
+//};
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+//app.use(cors(corsOptions));
+//app.options("*", cors(corsOptions));
 
 // Custom headers for preflight
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://icpar.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+//app.use((req, res, next) => {
+//  res.header("Access-Control-Allow-Origin", "https://icpar.vercel.app");
+// res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+//  res.header(
+//   "Access-Control-Allow-Headers",
+//   "Origin, X-Requested-With, Content-Type, Accept"
+// );
+// res.header("Access-Control-Allow-Credentials", "true");
+// next();
+//});
 
 // Built-in body parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "https://icpar.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Optional: File Upload Middleware Setup (e.g., using multer)
 // const multer = require("multer");
