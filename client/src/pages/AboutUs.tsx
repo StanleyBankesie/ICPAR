9eab28fb5f0e89dee81896024f99533cd523f270
STANLEY BANKESIE <stanleyetornam@gmail.com>
2025-05-23 20:02:40 +0000

    update

--- client/src/pages/AboutUs.tsx
+++ client/src/pages/AboutUs.tsx
@@ -193,21 +193,21 @@ const AboutUsPage: React.FC = () => {
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-20 mt-18">
             <div>
-              <div className="  bg-primary-100 rounded-full flex items-center justify-center mb-8 shadow-md py-8 px-2 mx-5">
+              <div className="  bg-primary-100 rounded-full flex items-center justify-center mb-8 shadow-md py-8 px-2 mx-10">
                 <span className="text-2xl text-primary-700">
                   Liberia Chapter
                 </span>
               </div>
             </div>
             <div>
-              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-8 shadow-md py-8 mx-5">
+              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-8 shadow-md py-8 mx-10">
                 <span className="text-2xl text-primary-700">
                   Ghana/Nigeria Chapter
                 </span>
               </div>
             </div>
             <div>
-              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-4 shadow-md py-8 mx-5">
+              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-4 shadow-md py-8 mx-10">
                 <span className="text-2xl text-primary-700">
                   Sierra Leone Chapter
                 </span>
