f8be6264d64788dff8dc4491741942ad03bf59ad
STANLEY BANKESIE <stanleyetornam@gmail.com>
2025-05-23 18:48:27 +0000

    update

--- client/src/pages/AboutUs.tsx
+++ client/src/pages/AboutUs.tsx
@@ -199,8 +199,8 @@ const AboutUsPage: React.FC = () => {
             </div>
 
             <div>
-              <div className=" text-3xl bg-primary-100 rounded-full flex items-center justify-center mb-4">
-                <span className="text-2xl text-primary-700 shadow-md">
+              <div className=" text-3xl bg-primary-100 rounded-full flex items-center justify-center mb-4 shadow-md">
+                <span className="text-2xl text-primary-700">
                   Ghana/Nigeria Chapter
                 </span>
               </div>
