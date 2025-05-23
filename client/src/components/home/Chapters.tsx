4a98d1698820f9dfa54e92f7c54d2b8e7cb3c443
STANLEY BANKESIE <stanleyetornam@gmail.com>
2025-05-23 20:15:07 +0000

    Update

---
+++ client/src/components/home/Chapters.tsx
@@ -0,0 +1,45 @@
+import SectionTitle from "../common/SectionTitle";
+const Chapters = () => {
+  return (
+    <div>
+      <section className="section">
+        <div className="container">
+          <SectionTitle title="ICPAR CHAPTERS" />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-20 mt-18">
+            <div>
+              <div className="  bg-primary-100 rounded-full flex items-center justify-center mb-8 shadow-md py-8 px-2 mx-10">
+                <span className="text-2xl text-primary-700">
+                  Liberia Chapter
+                </span>
+              </div>
+            </div>
+            <div>
+              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-8 shadow-md py-8 mx-10">
+                <span className="text-2xl text-primary-700">
+                  Ghana/Nigeria Chapter
+                </span>
+              </div>
+            </div>
+            <div>
+              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-4 shadow-md py-8 mx-10">
+                <span className="text-2xl text-primary-700">
+                  Sierra Leone Chapter
+                </span>
+              </div>
+            </div>
+            <div>
+              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-4 shadow-md py-8 mx-5">
+                <span className="text-2xl text-primary-700">
+                  USA/UK Chapter
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Chapters;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,12 +3,14 @@ import Hero from "../components/home/Hero";
 import AboutSection from "../components/home/AboutSection";
 import VideoSection from "../components/home/VideoSection";
 import PressReleaseSection from "../components/home/PressReleaseSection";
+import Chapters from "../components/home/Chapters";
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <Hero />
       <AboutSection />
+      <Chapters />
       <VideoSection />
       <PressReleaseSection />
     </div>
