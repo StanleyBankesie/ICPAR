e52e5334b54753c31bdea05ff17713ed774d49b1
STANLEY BANKESIE <stanleyetornam@gmail.com>
2025-05-23 23:11:18 +0000

    update

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import Hero from "../components/home/Hero";
 import AboutSection from "../components/home/AboutSection";
 import VideoSection from "../components/home/VideoSection";
 import PressReleaseSection from "../components/home/PressReleaseSection";
-import Chapter from "../components/home/Chapters";
+import Chapter from "../components/home/Chapter";
 
 const HomePage: React.FC = () => {
   return (
