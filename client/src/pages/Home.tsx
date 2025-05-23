eed831569574211c4bf184d6a6b166f4638ed770
STANLEY BANKESIE <stanleyetornam@gmail.com>
2025-05-23 23:21:39 +0000

    update

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,14 +3,14 @@ import Hero from "../components/home/Hero";
 import AboutSection from "../components/home/AboutSection";
 import VideoSection from "../components/home/VideoSection";
 import PressReleaseSection from "../components/home/PressReleaseSection";
-import Chapter from "../components/home/Chapter";
+import Chapters from "../components/home/Chapters";
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <Hero />
       <AboutSection />
-      <Chapter />
+      <Chapters />
       <VideoSection />
       <PressReleaseSection />
     </div>
