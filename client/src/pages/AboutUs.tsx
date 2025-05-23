ccf0d101b0bdbc7c4fa394b2dac8819be8b81171
STANLEY BANKESIE <stanleyetornam@gmail.com>
2025-05-23 19:58:10 +0000

    update

--- client/src/pages/AboutUs.tsx
+++ client/src/pages/AboutUs.tsx
@@ -191,7 +191,7 @@ const AboutUsPage: React.FC = () => {
         <div className="container">
           <SectionTitle title="ICPAR CHAPTERS" />
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-20 mt-18">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-30 mt-18">
             <div>
               <div className="  bg-primary-100 rounded-full flex items-center justify-center mb-8 shadow-md py-8 px-2 mx-5">
                 <span className="text-3xl text-primary-700">
@@ -200,22 +200,22 @@ const AboutUsPage: React.FC = () => {
               </div>
             </div>
             <div>
-              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-8 shadow-md py-10 mx-5">
-                <span className="text-3xl text-primary-700 font-semibold">
+              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-8 shadow-md py-8 mx-5">
+                <span className="text-3xl text-primary-700">
                   Ghana/Nigeria Chapter
                 </span>
               </div>
             </div>
             <div>
-              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-4 shadow-md py-10 mx-5">
-                <span className="text-3xl text-primary-700 font-semibold">
+              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-4 shadow-md py-8 mx-5">
+                <span className="text-3xl text-primary-700">
                   Sierra Leone Chapter
                 </span>
               </div>
             </div>
             <div>
-              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-4 shadow-md py-10 mx-5">
-                <span className="text-3xl text-primary-700 font-semibold">
+              <div className=" bg-primary-100 rounded-full flex items-center justify-center mb-4 shadow-md py-8 mx-5">
+                <span className="text-3xl text-primary-700">
                   USA/UK Chapter
                 </span>
               </div>
